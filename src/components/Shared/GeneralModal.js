48e4bc10db4edf6a0dc78d63c7fec82963593e35
Garth Toland <tolandgarth@gmail.com>
2020-02-20 23:39:30 +0000

    Modal tidy up

--- src/components/Shared/GeneralModal.js
+++ src/components/Shared/GeneralModal.js
@@ -11,7 +11,9 @@ const GeneralModal = ({
     <Modal.Header closeButton>
       <Modal.Title>{title}</Modal.Title>
     </Modal.Header>
-    <Modal.Body>{body}</Modal.Body>
+    <Modal.Body>
+      <p dangerouslySetInnerHTML={{ __html: body }} />
+    </Modal.Body>
     <Modal.Footer>
       <Button variant="secondary" onClick={onClose}>
         Close
